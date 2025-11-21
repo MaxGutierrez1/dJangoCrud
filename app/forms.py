2a16fae089e469a5105838099f5338a95fc3d0a0
“MaxGutierrez” <amaxghz@gmail.com>
2025-11-21 16:55:36 -0600

    correcting forms

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import Usuarios
+
 class UsuarioForm(forms.ModelForm):
     class Meta:
-        model=Usuarios
-        fields=['nombres','apellidos','profesion','correoElectronico']
+        model = Usuarios
+        fields = ['nombres', 'apellidos', 'profesion', 'correoElectronico']
 
-        #widgets
-        def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs) # Inicializa el formulario primero
-                
-                for field_name, field in self.fields.items():
-                    field.widget.attrs['class'] = 'w-full border'
-                    
-                    if field_name == 'email':
-                        field.widget.attrs['placeholder'] = 'tu@email.com'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'w-full border'
+            
+            if field_name == 'correoElectronico':
+                field.widget.attrs['placeholder'] = 'tu@email.com'
